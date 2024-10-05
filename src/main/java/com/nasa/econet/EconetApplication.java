5613489ac00612c6b9ecf904f20bc613d48b22e1
alexandr <Control de ventanaVista mvil Acerca dealeksandr.tarasov@fyg.ugr.es>
2024-10-05 10:08:34 +0200

    feat(main): initialize empty spring boot project with Thymeleaf

---
+++ src/main/java/com/nasa/econet/EconetApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.econet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconetApplication.class, args);
+	}
+
+}
