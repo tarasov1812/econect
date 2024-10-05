ebe89e08a33c505d201dda3bd2fccc6455c673e6
alexandr <Control de ventanaVista mvil Acerca dealeksandr.tarasov@fyg.ugr.es>
2024-10-05 14:50:43 +0200

    feat(main): get CO2 emission data from https://earth.gov/

--- src/main/java/com/nasa/econet/controllers/HomeController.java
+++ src/main/java/com/nasa/econet/controllers/HomeController.java
@@ -1,12 +1,27 @@
 package com.nasa.econet.controllers;
 
+import com.nasa.econet.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/dataCO2")
     public String home() {
-        return "index";
+        return "dataCO2";
+    }
+
+    @GetMapping("/profile")
+    public String userProfile(Model model) {
+        // Sample fictional user data
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("john_doe");
+        user.setEmail("john.doe@example.com");
+        user.setBio("Lorem ipsum dolor sit amet.");
+
+        model.addAttribute("user", user);
+        return "profile";
     }
 }
